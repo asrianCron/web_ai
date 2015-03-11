423e737516d8105f98ceacbda5a79a879f82b7fb
AdrianAntochi <adrian.constantin.antochi@gmail.com>
2015-03-11 14:42:49 +0200

    Trying to make dropdown work

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Move Around</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script src="js/jquery-2.1.3.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </head>
+    <body>
+
+        <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Behaviours
+            <span class="caret"></span>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+                <li role="presentation"> <a role="menuitem" tabindex="-1" href="#">Item</a> </li>
+                <li role="presentation"> <a role="menuitem" tabindex="-1" href="#">Item</a> </li>
+                <li role="presentation"> <a role="menuitem" tabindex="-1" href="#">Item</a> </li>
+            </ul>
+            </buton>
+
+        </div>
+
+    </body>
+</html>
+ 
